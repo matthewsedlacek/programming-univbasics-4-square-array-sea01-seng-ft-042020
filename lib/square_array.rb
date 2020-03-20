24f1137cabb080af30f215062ab97d23d5cd1dbc
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-20 17:19:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,10 @@
 numbers = [1,2,3]
   
-
 def square_array(numbers)
   counter = 0 
 
   while counter < numbers.length do
-  new_numbers = []
+  
   puts numbers[counter]
   new_numbers.push(numbers[counter])
   counter += 1
